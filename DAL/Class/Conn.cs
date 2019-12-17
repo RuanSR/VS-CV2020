377f296831a29c8a5fe07268abb1e759422811da
RuanSR <ruansilvarosa@gmail.com>
2019-12-16 23:43:14 -0200

    Iniciado primeiras configuracoes no DAL. Criado arquivo App.Config. Criado Classe de conexao com o DB. Criado Database file mdf.

---
+++ DAL/Class/Conn.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace DAL.Class
+{
+    public static class Conn
+    {
+        private static string _caminhoBanco = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "DBFile.mdf;");
+        public static string StrConn { get; private set; } = ConfigurationManager.ConnectionStrings["SQL-Server"].ToString() + _caminhoBanco;
+    }
+}
